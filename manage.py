d56f13ccf4c231a4115d1f4aae124ddc071a8dc6
Treemo <treemo@hotmail.fr>
2017-12-18 15:56:43 +0000

    Fix crash with alpine

--- manage.py
+++ manage.py
@@ -2,6 +2,13 @@
 import os
 import sys
 
+
+# Monkey patch for alpine
+if os.path.isfile('/etc/alpine-release'):
+    import threading
+    threading.stack_size(100000)
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mercure.settings")
 
